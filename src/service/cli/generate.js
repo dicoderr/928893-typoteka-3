8e9298eac99fbc54e83c4d5046469c0c7464ae19
Di Coderr <dicoderr@gmail.com>
2021-01-13 10:12:22 +0200

    Get mock data from files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -11,56 +11,9 @@ const FILE_NAME = `mocks.json`;
 const MAX_ANNOUNCE_LINES = 5;
 const MAX_MONTH_FROM_NOW = 3;
 const MS_IN_MONTH = Duration.fromObject({month: 1}).as(`milliseconds`);
-
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const LINES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const SENTENCES_PATH = `./data/sentences.txt`;
+const TITLES_PATH = `./data/titles.txt`;
+const CATEGORIES_PATH = `./data/categories.txt`;
 
 const getDate = () => {
   const now = Date.now();
@@ -69,12 +22,22 @@ const getDate = () => {
   return DateTime.fromMillis(ms);
 };
 
-const generatePosts = (count) => Array(count).fill(1).map(() => ({
-  title: TITLES[getRandomInt(0, TITLES.length - 1)],
+const readContent = async (path) => {
+  try {
+    const content = await fs.readFile(path, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generatePosts = (count, titles, categories, sentences) => Array(count).fill(1).map(() => ({
+  title: titles[getRandomInt(0, titles.length - 1)],
   createdDate: getDate().toFormat(`y-MM-dd hh:mm:ss`),
-  announce: getRandomRange(shuffle(LINES), 1, MAX_ANNOUNCE_LINES).join(` `),
-  fullText: getRandomRange(shuffle(LINES), MAX_ANNOUNCE_LINES).join(` `),
-  category: getRandomRange(shuffle(CATEGORIES)),
+  announce: getRandomRange(shuffle(sentences), 1, MAX_ANNOUNCE_LINES).join(` `),
+  fullText: getRandomRange(shuffle(sentences), MAX_ANNOUNCE_LINES).join(` `),
+  category: getRandomRange(shuffle(categories)),
 }));
 
 module.exports = {
@@ -87,8 +50,10 @@ module.exports = {
       console.info(chalk.blue(`Не больше 1000 публикаций`));
       return;
     }
-
-    const data = JSON.stringify(generatePosts(count));
+    const titles = await readContent(TITLES_PATH);
+    const categories = await readContent(CATEGORIES_PATH);
+    const sentences = await readContent(SENTENCES_PATH);
+    const data = JSON.stringify(generatePosts(count, titles, categories, sentences));
     try {
       await fs.writeFile(FILE_NAME, data);
       console.log(chalk.green(`Данные успешно записаны в файл.`));
