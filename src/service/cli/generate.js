d3def40bb5c6cede39687efe7281becb5f5e2aa0
Di Coderr <dicoderr@gmail.com>
2021-04-03 09:54:20 +0300

    Generate comments

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -10,11 +10,13 @@ const {getRandomInt, shuffle, getRandomRange} = require(`../../utils`);
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 const MAX_ANNOUNCE_LINES = 5;
+const MAX_COMMENTS = 2;
 const MAX_MONTH_FROM_NOW = 3;
 const MS_IN_MONTH = Duration.fromObject({month: 1}).as(`milliseconds`);
 const SENTENCES_PATH = `./data/sentences.txt`;
 const TITLES_PATH = `./data/titles.txt`;
 const CATEGORIES_PATH = `./data/categories.txt`;
+const COMMENTS_PATH = `./data/comments.txt`;
 const MAX_ID_LENGTH = 6;
 
 const getDate = () => {
@@ -34,7 +36,16 @@ const readContent = async (path) => {
   }
 };
 
-const generatePosts = (count, titles, categories, sentences) =>
+const generateComments = (count, comments) => {
+  return Array(count)
+    .fill(1)
+    .map(() => ({
+      id: nanoid(MAX_ID_LENGTH),
+      text: shuffle(comments).slice(0, getRandomInt(1, 3)).join(` `),
+    }));
+};
+
+const generatePosts = ({count, titles, categories, sentences, comments}) =>
   Array(count)
     .fill(1)
     .map(() => ({
@@ -44,6 +55,7 @@ const generatePosts = (count, titles, categories, sentences) =>
       announce: getRandomRange(shuffle(sentences), 1, MAX_ANNOUNCE_LINES).join(` `),
       fullText: getRandomRange(shuffle(sentences), MAX_ANNOUNCE_LINES).join(` `),
       category: getRandomRange(shuffle(categories)),
+      comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments),
     }));
 
 module.exports = {
@@ -59,7 +71,8 @@ module.exports = {
     const titles = await readContent(TITLES_PATH);
     const categories = await readContent(CATEGORIES_PATH);
     const sentences = await readContent(SENTENCES_PATH);
-    const data = JSON.stringify(generatePosts(count, titles, categories, sentences));
+    const comments = await readContent(COMMENTS_PATH);
+    const data = JSON.stringify(generatePosts({count, titles, categories, sentences, comments}));
     try {
       await fs.writeFile(FILE_NAME, data);
       console.log(chalk.green(`Данные успешно записаны в файл.`));
